1773215e97479ebe1f9acdbd3a741fa85e9fc3f4
sean <sdoody95@gmail.com>
2024-03-07 01:11:27 -0500

    updated recipe-search-results component to be reactive

--- src/app/services/edamam-api/edamam-api.service.ts
+++ src/app/services/edamam-api/edamam-api.service.ts
@@ -1,19 +1,21 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Recipe } from 'src/app/models/recipe.interface';
 import { SearchEvent } from 'src/app/models/search-event.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EdamamApiService {
-  appId: string = 'e553ac8f';
-  appKey: string = '4a65f97aed92762ca9818cfaef595dcf';
-  edamamUrl: string = 'https://api.edamam.com/api/recipes/v2';
+  private appId: string = 'e553ac8f';
+  private appKey: string = '4a65f97aed92762ca9818cfaef595dcf';
+  private edamamUrl: string = 'https://api.edamam.com/api/recipes/v2';
 
-  constructor(private http: HttpClient) {}
+  constructor(private httpClient: HttpClient) {}
 
-  getRecipes(searchEvent: SearchEvent): any {
-    return this.http.get(this.edamamUrl, {
+  public getRecipes(searchEvent: SearchEvent) {
+    const source$ = this.httpClient.get<any>(this.edamamUrl, {
       params: {
         app_id: this.appId,
         app_key: this.appKey,
@@ -24,15 +26,23 @@ export class EdamamApiService {
         health: searchEvent.dietaryRestrictions,
       },
     });
+
+    return source$.pipe(
+      map((result) => {
+        return result.hits.map((hit: any) => new Recipe('edamamApi', hit));
+      })
+    );
   }
 
-  getRecipeByUri(recipeUri: string): any {
-    return this.http.get(`${this.edamamUrl}/${recipeUri}`, {
+  public getRecipeByUri(recipeUri: string): Observable<Object> {
+    const source$ = this.httpClient.get(`${this.edamamUrl}/${recipeUri}`, {
       params: {
         app_id: this.appId,
         app_key: this.appKey,
         type: 'public',
       },
     });
+
+    return source$;
   }
 }
